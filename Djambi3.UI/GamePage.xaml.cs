5be1d786937c4f3d4fd860d9690bec6a94cc0c71
JamesFaix <jamesthefaix@gmail.com>
2018-03-20 05:35:44 -0400

    Got board drawing

--- Djambi3.UI/GamePage.xaml.cs
+++ Djambi3.UI/GamePage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Djambi.Engine;
 
 namespace Djambi.UI
 {
@@ -12,6 +16,49 @@ namespace Djambi.UI
         public GamePage()
         {
             InitializeComponent();
+            this.Loaded += (sender, e) =>
+            { 
+                Window window = Window.GetWindow(this);
+                window.SetBinding(Window.MinHeightProperty, new Binding() { Source = this.MinHeight });
+                window.SetBinding(Window.MinWidthProperty, new Binding() { Source = this.MinWidth });
+            };
+
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            for (var i = 0; i < Constants.BoardSize; i++)
+            {
+                gridBoard.RowDefinitions.Add(new RowDefinition
+                {
+                    Name = $"gridBoardRow{i}"
+                });
+                gridBoard.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Name = $"gridBoardColumn{i}"
+                });
+            }
+
+            for (var r = 0; r < Constants.BoardSize; r++)
+            {
+                for (var c = 0; c < Constants.BoardSize; c++)
+                {
+                    var color = (c + r) % 2 == 0
+                        ? Colors.Black
+                        : Colors.White;
+
+                    var rect = new Rectangle
+                    {
+                        Fill = new SolidColorBrush(color),
+                        Stretch = Stretch.Uniform,
+                    };
+
+                    gridBoard.Children.Add(rect);
+                    Grid.SetColumn(rect, c);
+                    Grid.SetRow(rect, r);
+                }
+            }
         }
 
         private void btnQuitToMenu_Click(object sender, RoutedEventArgs e)
