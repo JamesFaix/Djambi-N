857ba2aaa8d323b7d6ef8670a2ca55103c06cc4a
JamesFaix <jamesthefaix@gmail.com>
2018-03-25 11:46:26 -0400

    Created basic game UI

---
+++ client/unity/Djambi.Unity/Assets/Scripts/GameUIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Djambi.Engine;
+using Djambi.Engine.Extensions;
+using Djambi.Model;
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour {
+
+	void Start ()
+    {
+        PlacePiecesInInitialPositions(Controller.GameState);
+        AddEntriesToLog(Controller.GameState.Log);
+        Controller.GetValidSelections()
+            .OnValue(ShowSelectionsOptions)
+            .OnError(error => ShowError(error.Message));
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    private void PlacePiecesInInitialPositions(GameState game)
+    {
+
+    }
+
+    private void AddEntriesToLog(IEnumerable<string> messages)
+    {
+
+    }
+
+    private void ShowSelectionsOptions(IEnumerable<Selection> selections)
+    {
+
+    }
+
+    private void ShowError(string message)
+    {
+
+    }
+}
