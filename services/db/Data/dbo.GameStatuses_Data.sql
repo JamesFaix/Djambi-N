773f414b847dba79bfefe0ce0c0035a07e538844
James Faix <jamesthefaix@gmail.com>
2018-08-13 21:14:33 -0400

    Updated tables

---
+++ services/db/Data/dbo.GameStatuses_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO [dbo].[GameStatuses] ([GameStatusId], [Name]) VALUES (1, N'Open')
+INSERT INTO [dbo].[GameStatuses] ([GameStatusId], [Name]) VALUES (2, N'Started')
+INSERT INTO [dbo].[GameStatuses] ([GameStatusId], [Name]) VALUES (3, N'Complete')
+INSERT INTO [dbo].[GameStatuses] ([GameStatusId], [Name]) VALUES (4, N'Canceled')
