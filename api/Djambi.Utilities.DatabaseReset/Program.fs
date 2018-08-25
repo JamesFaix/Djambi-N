c5ce1c7d33db8d6542c5c50c6e3cd1ceb64240fc
James Faix <jamesthefaix@gmail.com>
2018-08-25 07:09:15 -0400

    Created database reset script

--- api/Djambi.Utilities.DatabaseReset/Program.fs
+++ api/Djambi.Utilities.DatabaseReset/Program.fs
@@ -1,8 +1,80 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.Configuration
+open System.Data.SqlClient
+open System.IO
+open System.Reflection
+open System.Text.RegularExpressions
 
-open System
+open Dapper
+
+let private getSqlDirectory =
+    let asm = Assembly.GetExecutingAssembly()
+    let uri = new Uri(asm.CodeBase)
+    let asmDir = uri.LocalPath |> Path.GetDirectoryName
+    let relativeDir = ConfigurationManager.AppSettings.["sqlDirectoryRelativePath"]
+    Path.Combine(asmDir, relativeDir)
+
+let private masterConnectionString = 
+    ConfigurationManager.ConnectionStrings.["master"].ConnectionString
+        
+let private djambiConnectionString = 
+    ConfigurationManager.ConnectionStrings.["djambi"].ConnectionString
+
+let private executeCommand (cnStr : string)(command : string) : unit =
+    use cn = new SqlConnection(cnStr)
+    cn.Execute(command) |> ignore
+    
+let private dropAndCreateDb() : unit =
+    printfn "Dropping and creating database"
+    let sql = "IF EXISTS(SELECT * FROM sys.databases WHERE name='Djambi')
+               DROP DATABASE Djambi;
+               CREATE DATABASE Djambi;"
+    executeCommand masterConnectionString sql
+
+let private loadFile (relativePath : string) : unit =
+    printfn "Loading %s" relativePath
+    let absolutePath = Path.Combine(getSqlDirectory, relativePath)
+    let text = File.ReadAllText(absolutePath)
+    let commands = Regex.Split(text, "\s+GO")
+                   |> Seq.filter (String.IsNullOrEmpty >> not)
+    for c in commands do    
+        executeCommand djambiConnectionString c
+
+let getFilesInOrder : string seq =
+    seq {
+        //Order is very important for foreign keys
+        let tables = [
+            "Users"
+            "GameStatuses"
+            "Games"
+            "Players"
+            "VirtualPlayerNames"
+            "Turns"
+            "Messages"        
+        ]
+
+        yield! tables |> Seq.map (fun name -> sprintf "Tables\\dbo.%s.sql" name)
+
+        let getFiles folder =
+            Path.Combine(getSqlDirectory, folder)
+            |> Directory.EnumerateFiles
+            |> Seq.map (fun path -> Path.Combine(folder, Path.GetFileName(path)))
+
+        let folders = [
+            "Security\\Schemas"
+            "Stored Procedures"
+            "Data"
+        ]
+ 
+        yield! folders |> Seq.collect getFiles
+    }
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    dropAndCreateDb()
+    for f in getFilesInOrder do
+        loadFile f
+    
+    printfn "Done"
+    Console.Read() |> ignore
+    0
