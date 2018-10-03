5231974391155bccf500ce55a3b684882849dfba
JamesFaix <jamesthefaix@gmail.com>
2018-10-03 02:32:26 -0400

    Fixed missing files for web build

--- docs/DevSetup.md
+++ docs/DevSetup.md
@@ -1,9 +1,15 @@
 ## Required Tools
 - .NET Core 2.1 SDK
 - SQL Server
+- NPM
+- Webpack + plugins
+    `npm install --save-dev webpack`
+    `npm install --save-dev typescript ts-loader`
+    `npm install --save-dev style-loader`
+    
 
 ## Environment configuration
-- At the repository root, create an `environment.json` file for some global configuration.
+- At the repository root, create an `environment.json` file for some global configuration. This file is not checked into source control, but is required.
 
 ```
 {
---
+++ web/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+      "index": "./src/js/pages/index.ts"
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+};
