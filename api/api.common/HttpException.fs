81a6d324fdae47f69c57be692e89a2b81056f817
JamesFaix <jamesthefaix@gmail.com>
2018-10-12 19:33:01 -0400

    Moved AsyncHttpResult type and module into same file

--- api/api.common/HttpException.fs
+++ api/api.common/HttpException.fs
@@ -1,11 +1,8 @@
 ﻿namespace Djambi.Api.Common
 
 open System
-open System.Threading.Tasks
 
 type HttpException(statusCode : int, message: string) =
     inherit Exception(message)
 
     member this.statusCode = statusCode
-    
-type AsyncHttpResult<'a> = Task<Result<'a, HttpException>>
