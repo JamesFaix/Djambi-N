6f112889cc74c6444da8e31d59d1c61b1f96d85f
JamesFaix <jamesthefaix@gmail.com>
2018-10-12 17:48:44 -0400

    Refactored result module

---
+++ api/api.common/Result.fs
@@ -0,0 +1,21 @@
+﻿module Djambi.Api.Common.Result
+
+let isOk (r : Result<'a, 'b>) = 
+    match r with
+    | Ok _ -> true
+    | _ -> false
+
+let isError (r : Result<'a, 'b>) =
+    match r with
+    | Ok _ -> false
+    | _ -> true
+
+let value (r : Result<'a, 'b>) =
+    match r with
+    | Ok x -> x
+    | _ -> raise <| invalidOp "Cannot get value of error result."
+
+let error (r : Result<'a, 'b>) =
+    match r with
+    | Error x -> x
+    | _ -> raise <| invalidOp "Cannot get error string of ok result."
