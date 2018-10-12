544e0ec106fa21ad0f8cfd385a8379ad677946df
JamesFaix <jamesthefaix@gmail.com>
2018-10-11 23:19:02 -0400

    Refactored SessionService

--- api/api.common/Task.fs
+++ api/api.common/Task.fs
@@ -24,11 +24,38 @@ module Task =
         t |> map (Result.bind projection)
 
     let thenBindAsync (projection : 'a -> 'b HttpResult Task) (t : 'a HttpResult Task) : 'b HttpResult Task =
+        let projectIfValue (result : 'a HttpResult) =
+            match result with
+            | Ok x -> projection x
+            | Error x -> Task.FromResult(Error x)
+
+        t |> bind projectIfValue
+        
+    let thenDoAsync (action : 'a -> _ HttpResult Task) (t : 'a HttpResult Task) : 'a HttpResult Task =
         task {
             let! result = t
             match result with
+            | Error _ -> return result   
             | Ok x -> 
-                return! projection x
-            | Error x -> 
-                return Error x                
+                let! actionResult = action x        
+                match actionResult with
+                | Ok _ -> return Ok x
+                | Error y -> return Error y                    
+        }
+    
+    let thenReplaceError (statusCode : int) (newException : HttpException) (t : 'a HttpResult Task) : 'a HttpResult Task =
+        let mapIfMatch (oldException : HttpException) =
+            match oldException with
+            | ex when ex.statusCode = statusCode -> newException
+            | _ -> oldException
+
+        t |> map (Result.mapError mapIfMatch)
+       
+    let thenBindError (statusCode : int) (projection : HttpException -> 'a HttpResult) (t : 'a HttpResult Task) : 'a HttpResult Task =
+        task {
+            let! result = t
+            match result with
+            | Error ex when ex.statusCode = statusCode -> 
+                return projection ex
+            | _ -> return result
         }
