327862716708c2596c02fd08e1efb8b3995e3aac
James Faix <jamesthefaix@gmail.com>
2019-01-29 00:04:37 -0500

    Ported click handler to web2

--- web2/src/components/gameBoard.tsx
+++ web2/src/components/gameBoard.tsx
@@ -3,6 +3,7 @@ import ApiClient from '../api/client';
 import { User, Game } from '../api/model';
 import BoardView from '../display/boardView';
 import BoardRenderer from '../display/boardRenderer';
+import { BoardClickHandler } from '../display/boardClickHandler';
 
 export interface GameBoardProps {
     user : User,
@@ -38,6 +39,9 @@ export default class GameBoard extends React.Component<GameBoardProps, GameBoard
         if (this.props.game && this.props.boardView) {
             const canvas = document.getElementById("canvas_board") as HTMLCanvasElement;
             const renderer = new BoardRenderer(canvas, this.props.boardView);
+            const clickHandler = new BoardClickHandler(renderer, canvas, this.props.boardView, this.props.api, this.props.game.id);
+            canvas.onclick = (e) => clickHandler.clickOnBoard(e);
+            renderer.onPieceClicked = (gameId, cellId) => clickHandler.clickOnCell(gameId, cellId);
             renderer.updateGame(this.props.game);
         }
     }
