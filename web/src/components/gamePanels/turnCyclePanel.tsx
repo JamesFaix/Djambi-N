51c6d90be551e98420520602d03009e6e92771b6
James Faix <jamesthefaix@gmail.com>
2019-01-31 10:41:54 -0500

    Created basic turn cycle panel

---
+++ web/src/components/gamePanels/turnCyclePanel.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import '../../index.css';
+import { Game } from '../../api/model';
+import ThemeService from '../../themes/themeService';
+
+export interface TurnCyclePanelProps {
+    game : Game,
+    theme : ThemeService
+}
+
+export default class TurnCyclePanel extends React.Component<TurnCyclePanelProps> {
+
+    private readonly scale = 50;
+
+    render() {
+        const containerStyle = {
+            display: "flex"
+        };
+
+        return (
+            <div className="thinBorder">
+                Turn Cycle
+                <div style={containerStyle}>
+                    {
+                        this.getPlayerViews()
+                            .map(pv => {
+                                const style = {
+                                    background: pv.color,
+                                    height: this.scale,
+                                    width: this.scale,
+                                    "text-align": "center",
+                                };
+                                return (
+                                    <div
+                                        className="thinBorder"
+                                        style={style}>
+                                        {pv.id}
+                                    </div>
+                                );
+                            })
+                    }
+                </div>
+            </div>
+        );
+    }
+
+    private getPlayerViews() : PlayerView[] {
+        const game = this.props.game;
+
+        return game.turnCycle
+            .map(pId => {
+                const p = game.players.find(p => p.id === pId);
+                return {
+                    id: p.id,
+                    name: p.name,
+                    color: this.props.theme.getPlayerColor(p.colorId)
+                }
+            });
+    }
+}
+
+interface PlayerView {
+    id : number,
+    name : string,
+    color : string,
+}
