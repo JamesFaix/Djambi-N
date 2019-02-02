701f183fc32fcdee08a133406285f75c9b790483
James Faix <jamesthefaix@gmail.com>
2019-02-02 10:24:22 -0500

    Fixed union serialization

--- api/api.common/Json/SingleFieldUnionJsonConverter.fs
+++ api/api.common/Json/SingleFieldUnionJsonConverter.fs
@@ -4,6 +4,7 @@ open System
 open System.IO
 open FSharp.Reflection
 open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 open Djambi.Api.Common.Collections
 
 type SingleFieldUnionJsonConverter() =
@@ -20,85 +21,28 @@ type SingleFieldUnionJsonConverter() =
 
         let caseType = value.GetType()
         let (case, _) = FSharpValue.GetUnionFields(value, caseType)
+        let itemProp = caseType.GetProperty "Item"
+        let recordValue = itemProp.GetValue value
+
         writer.WriteStartObject()
         writer.WritePropertyName "kind"
         writer.WriteValue case.Name
         writer.WritePropertyName "value"
-
-        let itemProp = caseType.GetProperty "Item"
-        let recordValue = itemProp.GetValue value
-
         serializer.Serialize(writer, recordValue)
-
         writer.WriteEndObject()
 
     override x.ReadJson(reader : JsonReader, 
                         t : Type, 
                         existingValue : obj,
                         serializer : JsonSerializer) : obj =   
-                        
-        let tokens = ArrayList<JsonToken * obj>()
-        let mutable objStarts = 1
-        let mutable objEnds = 0
-        tokens.Add (reader.TokenType, reader.Value)
-
-        while objStarts > objEnds && reader.Read() do
-            let tt = reader.TokenType
-            if tt = JsonToken.StartObject then objStarts <- objStarts + 1
-            if tt = JsonToken.EndObject then objEnds <- objEnds + 1
-            tokens.Add (tt, reader.Value)
-
-        //Remove first and last tokens
-        tokens.RemoveAt(0)
-        tokens.RemoveAt(tokens.Count-1)
-
-        //Find index of first token that is a property named kind
-        let index = 
-            tokens 
-            |> Seq.findIndex (fun (t, v) -> 
-                t = JsonToken.PropertyName 
-                    && v.ToString() = "kind"
-            )
-
-        let (_, kind) = tokens.[index+1]
-
-        //Remove `kind` property
-        tokens.RemoveAt(index+1)
-        tokens.RemoveAt(index)
-
-        //Remove `value` property name
-        let index = 
-            tokens
-            |> Seq.findIndex (fun (t, v) -> 
-                t = JsonToken.PropertyName
-                    && v.ToString() = "value"
-            )
-
-        tokens.RemoveAt(index)
-
-        let json = 
-            let jsonParts = 
-                tokens
-                |> Seq.map (fun (t, v) ->
-                    match t with
-                    | JsonToken.StartObject -> "{"
-                    | JsonToken.EndObject -> "},"
-                    | JsonToken.PropertyName -> sprintf "\"%s\":" (v.ToString())
-                    | _ -> if v = null then "null," 
-                           else sprintf "\"%s\"," (v.ToString())
-                )
-
-            String.Join("", jsonParts)
-                .Replace(",}", "}")
-
-        let json = json.Substring(0, json.Length-1) //Remove trailing comma
-
+        let jobj = JObject.Load(reader)
+        let kind = jobj.["kind"].ToString()
+       
         let cases = FSharpType.GetUnionCases t
-        let case = cases |> Seq.find (fun c -> c.Name = kind.ToString())
-        let fields = case.GetFields()
-        let recordType = fields.[0].PropertyType
+        let case = cases |> Seq.find (fun c -> c.Name = kind)
+        let recordType = case.GetFields().[0].PropertyType
 
-        use reader = new StringReader(json)
+        use reader = new StringReader(jobj.["value"].ToString())
         let record = serializer.Deserialize(reader, recordType)
         let union = FSharpValue.MakeUnion(case, [|record|])
 
