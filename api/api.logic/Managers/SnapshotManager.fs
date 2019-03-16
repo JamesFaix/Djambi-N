8233cb494d9910fe8b7be6af76f68e9a5e0c22d9
James Faix <jamesthefaix@gmail.com>
2019-03-16 07:59:48 -0400

    Turned managers into classes

--- api/api.logic/Managers/SnapshotManager.fs
+++ api/api.logic/Managers/SnapshotManager.fs
@@ -1,55 +1,53 @@
-﻿module Djambi.Api.Logic.Managers.SnapshotManager
+﻿namespace Djambi.Api.Logic.Managers
 
 open Djambi.Api.Common.Control  
 open Djambi.Api.Common.Control.AsyncHttpResult
-open Djambi.Api.Logic
 open Djambi.Api.Logic.Services
 open Djambi.Api.Model
-open Djambi.ClientGenerator.Annotations
 open Djambi.Ap.Db.Repositories
 open Djambi.Api.Db.Repositories
+open Djambi.Api.Logic.Interfaces
 
-[<ClientFunction(HttpMethod.Post, Routes.snapshots, ClientSection.Snapshots)>]
-let createSnapshot (gameId : int) (request : CreateSnapshotRequest) (session : Session) : SnapshotInfo AsyncHttpResult =
-    SecurityService.ensureHas Privilege.Snapshots session
-    |> Result.bindAsync (fun _ ->
-        GameRepository.getGame gameId
-        |> thenBindAsync (fun game -> 
-            EventRepository.getEvents (gameId, EventsQuery.empty)
-            |> thenBindAsync (fun history -> 
-                let request = 
-                    {
-                        game = game
-                        history = history
-                        description = request.description
-                        createdByUserId = session.user.id
-                    }
-                SnapshotRepository.createSnapshot request
+type SnapshotManager() =
+    interface ISnapshotManager with
+
+        member x.createSnapshot gameId request session =
+            SecurityService.ensureHas Privilege.Snapshots session
+            |> Result.bindAsync (fun _ ->
+                GameRepository.getGame gameId
+                |> thenBindAsync (fun game -> 
+                    EventRepository.getEvents (gameId, EventsQuery.empty)
+                    |> thenBindAsync (fun history -> 
+                        let request = 
+                            {
+                                game = game
+                                history = history
+                                description = request.description
+                                createdByUserId = session.user.id
+                            }
+                        SnapshotRepository.createSnapshot request
+                    )
+                )
+                |> thenBindAsync (fun snapshotId ->
+                    SnapshotRepository.getSnapshot snapshotId
+                    |> thenMap Snapshot.hideDetails
+                )
             )
-        )
-        |> thenBindAsync (fun snapshotId ->
-            SnapshotRepository.getSnapshot snapshotId
-            |> thenMap Snapshot.hideDetails
-        )
-    )
     
-[<ClientFunction(HttpMethod.Get, Routes.snapshots, ClientSection.Snapshots)>]
-let getSnapshotsForGame (gameId : int) (session : Session) : SnapshotInfo list AsyncHttpResult =
-    SecurityService.ensureHas Privilege.Snapshots session
-    |> Result.bindAsync (fun _ ->
-        SnapshotRepository.getSnapshotsForGame gameId
-    )
+        member x.getSnapshotsForGame gameId session =
+            SecurityService.ensureHas Privilege.Snapshots session
+            |> Result.bindAsync (fun _ ->
+                SnapshotRepository.getSnapshotsForGame gameId
+            )
     
-[<ClientFunction(HttpMethod.Delete, Routes.snapshot, ClientSection.Snapshots)>]
-let deleteSnapshot (gameId : int) (snapshotId : int) (session : Session) : Unit AsyncHttpResult =
-    SecurityService.ensureHas Privilege.Snapshots session
-    |> Result.bindAsync (fun _ ->
-        SnapshotRepository.deleteSnapshot snapshotId
-    )
+        member x.deleteSnapshot gameId snapshotId session =
+            SecurityService.ensureHas Privilege.Snapshots session
+            |> Result.bindAsync (fun _ ->
+                SnapshotRepository.deleteSnapshot snapshotId
+            )
 
-[<ClientFunction(HttpMethod.Post, Routes.snapshotLoad, ClientSection.Snapshots)>]
-let loadSnapshot (gameId : int) (snapshotId : int) (session : Session) : Unit AsyncHttpResult =
-    SecurityService.ensureHas Privilege.Snapshots session
-    |> Result.bindAsync (fun _ ->
-        SnapshotRepository.loadSnapshot (gameId, snapshotId)
-    )
+        member x.loadSnapshot gameId snapshotId session =
+            SecurityService.ensureHas Privilege.Snapshots session
+            |> Result.bindAsync (fun _ ->
+                SnapshotRepository.loadSnapshot (gameId, snapshotId)
+            )
--- api/api.logic/Marker.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Djambi.Api.Logic
-
-//This is a marker so typeof can be used to get a reference 
-//to the containing assembly without dynamically loading assemblies
-type Marker = { a : int }
