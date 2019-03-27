c35f62cf6bfbf4d859f6f6e319d6aeb59f3bfc13
James Faix <jamesthefaix@gmail.com>
2019-03-27 07:26:00 -0400

    Fixed DI problem

--- api/api.db/DbRoot.fs
+++ api/api.db/DbRoot.fs
@@ -5,11 +5,17 @@ open Djambi.Api.Db.Interfaces
 
 type DbRoot(connectionString : string) =
     let ctxProvider = CommandContextProvider(connectionString)
-    member x.games = GameRepository(ctxProvider)
-    member x.users = UserRepository(ctxProvider)
-    member x.events = EventRepository(ctxProvider, x.games)
-    member x.sessions = SessionRepository(ctxProvider, x.users)
-    member x.snapshots = SnapshotRepository(ctxProvider)
+    let _games = GameRepository(ctxProvider)
+    let _users = UserRepository(ctxProvider)
+    let _events = EventRepository(ctxProvider, _games)
+    let _sessions = SessionRepository(ctxProvider, _users)
+    let _snapshots = SnapshotRepository(ctxProvider)
+
+    member x.events = _events
+    member x.games = _games
+    member x.sessions = _sessions
+    member x.snapshots = _snapshots
+    member x.users = _users
 
     interface IDbRoot with  
         member x.events = x.events :> IEventRepository
