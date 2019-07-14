4fa9029d2575a67c4cc778e77bef45e501baade9
James Faix <jamesthefaix@gmail.com>
2019-07-13 20:46:11 -0400

    Revert "Updated NPM packages due to vulnerability warnings"

--- web/test/logicTests.ts
+++ web/test/logicTests.ts
@@ -1,25 +1,24 @@
 import { expect } from 'chai';
 import Logic from '../src/logic';
-import 'mocha';
 
 describe('Logic.Xor', () => {
     it('Returns true if only A is true', () => {
-        let actual = Logic.xor(true, false);
+        let actual = Logic.Xor(true, false);
         expect(actual).to.equal(true);
     });
 
     it('Returns true if only B is true', () => {
-        let actual = Logic.xor(false, true);
+        let actual = Logic.Xor(false, true);
         expect(actual).to.equal(true);
     });
 
     it('Returns false if A and B are true', () => {
-        let actual = Logic.xor(true, true);
+        let actual = Logic.Xor(true, true);
         expect(actual).to.equal(false);
     });
 
     it('Returns false if A and B are false', () => {
-        let actual = Logic.xor(false, false);
+        let actual = Logic.Xor(false, false);
         expect(actual).to.equal(false);
     });
 });
@@ -27,25 +26,25 @@ describe('Logic.Xor', () => {
 describe('Logic.Xors', () => {
     it('Returns false if zero trues', () => {
         let xs = [false, false, false, false];
-        let actual = Logic.xors(xs);
+        let actual = Logic.Xors(xs);
         expect(actual).to.equal(false);
     });
 
     it('Returns true if one true', () => {
         let xs = [false, true, false, false];
-        let actual = Logic.xors(xs);
+        let actual = Logic.Xors(xs);
         expect(actual).to.equal(true);
     });
 
     it('Returns false if two trues', () => {
         let xs = [false, true, true, false];
-        let actual = Logic.xors(xs);
+        let actual = Logic.Xors(xs);
         expect(actual).to.equal(false);
     });
 
     it('Returns false if empty list', () => {
         let xs : boolean[] = [];
-        let actual = Logic.xors(xs);
+        let actual = Logic.Xors(xs);
         expect(actual).to.equal(false);
     });
 });
