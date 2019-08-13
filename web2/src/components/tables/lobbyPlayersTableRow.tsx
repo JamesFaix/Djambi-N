1a171837938fe10c6ef9049d36dedb59fafaa142
James Faix <jamesthefaix@gmail.com>
2019-08-13 04:54:01 -0400

    Split lobby players table row component into sub components

--- web2/src/components/tables/lobbyPlayersTableRow.tsx
+++ web2/src/components/tables/lobbyPlayersTableRow.tsx
@@ -12,135 +12,202 @@ interface LobbyPlayersTableRowProps {
     removePlayer : (gameId : number, playerId : number) => void
 }
 
-interface LobbyPlayersTableRowState {
-    guestName : string
-}
-
-export default class LobbyPlayersTableRow extends React.Component<LobbyPlayersTableRowProps, LobbyPlayersTableRowState> {
-    constructor(props : LobbyPlayersTableRowProps) {
-        super(props);
-        this.state = {
-            guestName : ""
-        };
-    }
-
+export default class LobbyPlayersTableRow extends React.Component<LobbyPlayersTableRowProps> {
     render() {
         const seat = this.props.seat;
 
         const style : React.CSSProperties = {};
 
-        if (seat.player.colorId) {
+        if (seat.player && seat.player.colorId) {
             const color = Colors.getColorFromPlayerColorId(seat.player.colorId);
             style.boxShadow = `inset 0 0 0 3px ${color}`;
         }
 
+        const isPending = this.props.game.status === GameStatus.Pending;
+
         switch (seat.action) {
             case LobbySeats.SeatActionType.None:
-                const playerName = seat.player
-                    ? seat.player.name
-                    : "(Empty)";
-
                 return (
-                    <tr style={style}>
-                        <td>{playerName}</td>
-                        <td>{seat.note}</td>
-                        {this.renderIfPending(<td></td>)}
-                    </tr>
+                    <LobbyNoneRow
+                        style={style}
+                        isGamePending={isPending}
+                        seat={seat}
+                    />
                 );
 
             case LobbySeats.SeatActionType.Join:
                 return (
-                    <tr style={style}>
-                        <td>(Empty)</td>
-                        <td></td>
-                        {this.renderIfPending(
-                            <td>
-                                <button
-                                    onClick={() => this.addSelfOnClick()}
-                                >
-                                    Join
-                                </button>
-                            </td>
-                        )}
-                    </tr>
+                    <LobbyJoinRow
+                        style={style}
+                        isGamePending={isPending}
+                        onAddSelfClicked={() => {
+                            const request : CreatePlayerRequest = {
+                                userId: this.props.currentUser.id,
+                                name: null,
+                                kind: PlayerKind.User
+                            };
+                            this.props.addPlayer(this.props.game.id, request);
+                        }}
+                    />
                 );
 
             case LobbySeats.SeatActionType.AddGuest:
                 return (
-                    <tr style={style}>
-                        <td>
-                            <input
-                                type="text"
-                                value={this.state.guestName}
-                                onChange={e => this.addGuestOnChanged(e)}
-                            />
-                        </td>
-                        <td></td>
-                        {this.renderIfPending(
-                            <td>
-                                <button
-                                    onClick={() => this.addGuestOnClick()}
-                                >
-                                    Add guest
-                                </button>
-                            </td>
-                        )}
-                    </tr>
+                    <LobbyAddGuestRow
+                        style={style}
+                        isGamePending={isPending}
+                        onAddGuestClicked={(guestName: string) => {
+                            const request : CreatePlayerRequest = {
+                                userId: this.props.currentUser.id,
+                                name: guestName,
+                                kind: PlayerKind.Guest
+                            };
+                            this.props.addPlayer(this.props.game.id, request);
+                        }}
+                    />
                 );
 
             case LobbySeats.SeatActionType.Remove:
                 return (
-                    <tr style={style}>
-                        <td>{seat.player.name}</td>
-                        <td>{seat.note}</td>
-                        {this.renderIfPending(
-                            <td>
-                                <button
-                                    onClick={() => this.removeOnClick(seat.player.id)}
-                                >
-                                    {LobbySeats.isSeatSelf(seat, this.props.currentUser) ? "Quit" : "Remove"}
-                                </button>
-                            </td>
-                        )}
-                    </tr>
+                    <LobbyRemoveRow
+                        style={style}
+                        isGamePending={isPending}
+                        seat={seat}
+                        currentUser={this.props.currentUser}
+                        onRemoveClicked={(playerId : number) =>
+                            this.props.removePlayer(this.props.game.id, playerId)}
+                    />
                 );
 
             default:
                 throw "Invalid SeatActionType";
         }
     }
+}
 
-    private renderIfPending(element : JSX.Element) : JSX.Element {
-        return this.props.game.status === GameStatus.Pending ? element : null;
+interface LobbyJoinRowProps {
+    style : React.CSSProperties,
+    isGamePending : boolean,
+    onAddSelfClicked : () => void
+}
+
+class LobbyJoinRow extends React.Component<LobbyJoinRowProps> {
+    render() {
+        return (
+            <tr style={this.props.style}>
+                <td>(Empty)</td>
+                <td></td>
+                {this.props.isGamePending ?
+                    <td>
+                        <button
+                            onClick={() => this.props.onAddSelfClicked()}
+                        >
+                            Join
+                        </button>
+                    </td>
+                : null}
+            </tr>
+        );
     }
+}
 
-    private addSelfOnClick() : void {
-        const request : CreatePlayerRequest = {
-            userId: this.props.currentUser.id,
-            name: null,
-            kind: PlayerKind.User
+interface LobbyAddGuestRowProps {
+    style : React.CSSProperties,
+    isGamePending : boolean,
+    onAddGuestClicked : (name : string) => void
+}
+
+interface LobbyAddGuestRowState {
+    guestName : string
+}
+
+class LobbyAddGuestRow extends React.Component<LobbyAddGuestRowProps, LobbyAddGuestRowState> {
+    constructor(props: LobbyAddGuestRowProps) {
+        super(props);
+        this.state = {
+            guestName: ""
         };
-
-        this.props.addPlayer(this.props.game.id, request);
     }
 
-    private addGuestOnClick() : void {
-        const request : CreatePlayerRequest = {
-            userId: this.props.currentUser.id,
-            name: this.state.guestName,
-            kind: PlayerKind.Guest
-        };
-
-        this.props.addPlayer(this.props.game.id, request);
-        this.setState({guestName: ""});
+    render() {
+        return (
+            <tr style={this.props.style}>
+                <td>
+                    <input
+                        type="text"
+                        value={this.state.guestName}
+                        onChange={e => this.setState({guestName: e.target.value})}
+                    />
+                </td>
+                <td></td>
+                {this.props.isGamePending ?
+                    <td>
+                        <button
+                            onClick={() => {
+                                this.setState({guestName: ""});
+                                this.props.onAddGuestClicked(this.state.guestName);
+                            }}
+                        >
+                            Add guest
+                        </button>
+                    </td>
+                : null}
+            </tr>
+        );
     }
+}
 
-    private addGuestOnChanged(event : React.ChangeEvent<HTMLInputElement>) : void {
-        const name = event.target.value;
-        this.setState({ guestName: name });
+interface LobbyRemoveRowProps {
+    style : React.CSSProperties,
+    isGamePending : boolean,
+    seat : Seat,
+    currentUser : User,
+    onRemoveClicked : (playerId : number) => void
+}
+
+class LobbyRemoveRow extends React.Component<LobbyRemoveRowProps> {
+    render() {
+        const seat = this.props.seat;
+        return (
+            <tr style={this.props.style}>
+                <td>{seat.player.name}</td>
+                <td>{seat.note}</td>
+                {this.props.isGamePending ?
+                    <td>
+                        <button
+                            onClick={() => this.props.onRemoveClicked(seat.player.id)}
+                        >
+                            {LobbySeats.isSeatSelf(seat, this.props.currentUser) ? "Quit" : "Remove"}
+                        </button>
+                    </td>
+                : null}
+            </tr>
+        );
     }
+}
 
-    private removeOnClick(playerId : number) : void {
-        this.props.removePlayer(this.props.game.id, playerId);
+interface LobbyNoneRowProps {
+    style : React.CSSProperties,
+    isGamePending : boolean,
+    seat : Seat
+}
+
+class LobbyNoneRow extends React.Component<LobbyNoneRowProps> {
+    render() {
+        const seat = this.props.seat;
+
+        const playerName = seat.player
+            ? seat.player.name
+            : "(Empty)";
+
+        return (
+            <tr style={this.props.style}>
+                <td>{playerName}</td>
+                <td>{seat.note}</td>
+                {this.props.isGamePending ?
+                    <td></td>
+                : null}
+            </tr>
+        );
     }
 }
