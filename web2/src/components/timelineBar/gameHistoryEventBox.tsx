8b355540c3b99203e60d7ad1b23425be0b1d1d16
James Faix <jamesthefaix@gmail.com>
2019-08-29 23:04:30 -0400

    Filled out event and effect copy

--- web2/src/components/timelineBar/gameHistoryEventBox.tsx
+++ web2/src/components/timelineBar/gameHistoryEventBox.tsx
@@ -1,57 +1,47 @@
 import * as React from 'react';
-import { Event, Game } from "../../api/model";
+import { Event, Game, Board } from "../../api/model";
 import GameHistoryEffectBox from './gameHistoryEffectBox';
 import { Classes } from '../../styles/styles';
 import GameHistory from '../../viewModel/gameHistory';
+import * as Copy from '../../utilities/copy';
 
 interface GameHistoryEventBoxProps {
     game : Game, //This will be needed eventually to get the correct player names from playerIDs in event objects
-    event : Event
+    event : Event,
+    board : Board
 }
 
-export default class GameHistoryEventBox extends React.Component<GameHistoryEventBoxProps> {
-    render() {
-        const e = this.props.event;
-        if (!e) {
-            return null;
-        }
+const GameHistoryEventBox : React.SFC<GameHistoryEventBoxProps> = props => {
+    const e = props.event;
+    if (!e) {
+        return null;
+    }
 
-        const p = this.props.game.players.find(p => p.id === e.actingPlayerId);
-        const agentName = this.getAgentName();
+    const p = props.game.players.find(p => p.id === e.actingPlayerId);
 
-        return (
-            <div
-                className={p ? Classes.playerBox : Classes.box}
-                data-player-color-id={p ? p.colorId : null}
-            >
-                {`Event ${e.id} - ${e.createdBy.time}`}<br/>
-                {`${agentName} did a ${e.kind}.`}<br/>
-                Effects:<br/>
-                <div className={Classes.indented}>
-                    {e.effects
-                        .filter(f => GameHistory.isDisplayableEffect(f))
-                        .map((f, i) => {
-                        return (
-                            <GameHistoryEffectBox
-                                game={this.props.game}
-                                effect={f}
-                                key={i}
-                            />
-                        );
-                    })}
-                </div>
+    return (
+        <div
+            className={p ? Classes.playerBox : Classes.box}
+            data-player-color-id={p ? p.colorId : null}
+        >
+            {`Event ${e.id} - ${e.createdBy.time}`}<br/>
+            {Copy.getEventDescription(e, props.game)}<br/>
+            <div className={Classes.indented}>
+                {e.effects
+                    .filter(f => GameHistory.isDisplayableEffect(f))
+                    .map((f, i) => {
+                    return (
+                        <GameHistoryEffectBox
+                            game={props.game}
+                            effect={f}
+                            key={i}
+                            board={props.board}
+                        />
+                    );
+                })}
             </div>
-        );
-    }
-
-    private getAgentName() : string {
-        const e = this.props.event;
-        if (e.actingPlayerId) {
-            return this.props.game.players.find(p => p.id === e.actingPlayerId).name;
-        } else if (e.createdBy.userId) {
-            return e.createdBy.userName;
-        } else {
-            return "System";
-        }
-    }
+        </div>
+    );
 }
+
+export default GameHistoryEventBox;
--- web2/src/components/timelineBar/gameHistorySection.tsx
+++ web2/src/components/timelineBar/gameHistorySection.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Event, Game } from "../../api/model";
+import { Event, Game, Board } from "../../api/model";
 import { State } from '../../store/root';
 import { connect } from 'react-redux';
 import GameHistoryEventBox from './gameHistoryEventBox';
@@ -9,39 +9,48 @@ import { Icons } from '../../utilities/icons';
 
 interface GameHistorySectionProps {
     game : Game,
-    history : Event[]
+    history : Event[],
+    board : Board
 }
 
-class gameHistorySection extends React.Component<GameHistorySectionProps> {
-    render() {
-        if (!this.props.history || !this.props.game) {
-            return null;
-        }
-
-        return (
-            <div style={Styles.historyContainer}>
-                <TimelineHeader icon={Icons.Timeline.history}/>
-                {
-                    this.props.history.map((e, i) => {
-                        return (
-                            <GameHistoryEventBox
-                                event={e}
-                                game={this.props.game}
-                                key={i}
-                            />
-                        );
-                    })
-                }
-            </div>
-        );
+const gameHistorySection : React.SFC<GameHistorySectionProps> = props => {
+    if (!props.history || !props.game) {
+        return null;
     }
+
+    return (
+        <div style={Styles.historyContainer}>
+            <TimelineHeader icon={Icons.Timeline.history}/>
+            {
+                props.history.map((e, i) => {
+                    return (
+                        <GameHistoryEventBox
+                            event={e}
+                            game={props.game}
+                            key={i}
+                            board={props.board}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 const mapStateToProps = (state : State) => {
-    return {
-        game: state.activeGame.game,
-        history: state.activeGame.history
-    };
+    if (state.activeGame.game){
+        return {
+            game: state.activeGame.game,
+            history: state.activeGame.history,
+            board : state.boards.boards.get(state.activeGame.game.parameters.regionCount)
+        };
+    } else {
+        return {
+            game : null,
+            history : null,
+            board : null
+        };
+    }
 };
 
 const GameHistorySection = connect(mapStateToProps)(gameHistorySection);
