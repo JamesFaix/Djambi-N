51e17347b2ba2045cc4ba1a9b8b10bfb20a6f831
James Faix <jamesthefaix@gmail.com>
2019-09-11 17:02:26 -0400

    Refactored navigation section

--- web/src/components/topBar/navigationSection.tsx
+++ web/src/components/topBar/navigationSection.tsx
@@ -28,109 +28,137 @@ interface NavigationOptions {
     gameId : number
 }
 
-function getOptionsFromProps(props : NavigationSectionProps) : NavigationOptions {
-    const route : string = props.location.pathname;
+const defaultOptions : NavigationOptions = {
+    showLogin: ButtonState.Hidden,
+    showSignup: ButtonState.Hidden,
+    showHome: ButtonState.Hidden,
+    showCreateGame: ButtonState.Hidden,
+    showLobby: ButtonState.Hidden,
+    showPlay: ButtonState.Hidden,
+    showDiplomacy: ButtonState.Hidden,
+    showSnapshots: ButtonState.Hidden,
+    showSettings: ButtonState.Hidden,
+    showGameOver: ButtonState.Hidden,
+    gameId: null
+}
 
-    const o : NavigationOptions = {
-        showLogin: ButtonState.Hidden,
-        showSignup: ButtonState.Hidden,
-        showHome: ButtonState.Hidden,
-        showCreateGame: ButtonState.Hidden,
-        showLobby: ButtonState.Hidden,
-        showPlay: ButtonState.Hidden,
-        showDiplomacy: ButtonState.Hidden,
-        showSnapshots: ButtonState.Hidden,
-        showSettings: ButtonState.Hidden,
-        showGameOver: ButtonState.Hidden,
-        gameId: null
-    };
+enum ContextType {
+    LoggedOut,
+    LoggedInWithNoActiveGame,
+    LoggedInWithActiveGame
+}
 
-    if (route === Routes.login) {
-        o.showLogin = ButtonState.Active;
+function getContextType(props : NavigationSectionProps) : ContextType {
+    const loc : string = props.location.pathname;
+    if (loc === "login" || loc === "signup") {
+        return ContextType.LoggedOut;
+    }
+    else if (loc.startsWith("/games") || props.game) {
+        return ContextType.LoggedInWithActiveGame;
+    }
+    else {
+        return ContextType.LoggedInWithNoActiveGame;
+    }
+}
+
+function applyDefaultsForContext(props : NavigationSectionProps, o : NavigationOptions) : void {
+    const type = getContextType(props);
+    if (type === ContextType.LoggedOut) {
         o.showSignup = ButtonState.Inactive;
-    }
-
-    else if (route === Routes.signup) {
-        o.showSignup = ButtonState.Active;
         o.showLogin = ButtonState.Inactive;
+    } else {
+        if (type === ContextType.LoggedInWithActiveGame ||
+            type === ContextType.LoggedInWithNoActiveGame) {
+            o.showHome = ButtonState.Inactive;
+            o.showCreateGame = ButtonState.Inactive;
+            o.showSettings = ButtonState.Inactive;
+        }
+
+        if (type === ContextType.LoggedInWithActiveGame) {
+            o.showLobby = ButtonState.Inactive;
+        }
+    }
+}
+
+function markCurrentPageActive(props : NavigationSectionProps, o : NavigationOptions) : void {
+    const loc : string = props.location.pathname;
+    if (loc.startsWith("/games")) {
+        const parts = loc.split("/");
+        const gamePage = parts[3]; //[ "", "games", "{id}" "{page}" ]
+        switch(gamePage) {
+            case "lobby":
+                o.showLobby = ButtonState.Active;
+                break;
+            case "play":
+                o.showPlay = ButtonState.Active;
+                break;
+            case "diplomacy":
+                o.showDiplomacy = ButtonState.Active;
+                break;
+            case "snapshots":
+                o.showSnapshots = ButtonState.Active;
+                break;
+            case "results":
+                o.showGameOver = ButtonState.Active;
+                break;
+        }
+    } else {
+        switch (loc) {
+            case Routes.login:
+                o.showLogin = ButtonState.Active;
+                break;
+            case(Routes.signup):
+                o.showSignup = ButtonState.Active;
+                break;
+            case(Routes.dashboard) :
+                o.showHome = ButtonState.Active;
+                break;
+            case(Routes.createGame) :
+                o.showCreateGame = ButtonState.Active;
+                break;
+            case(Routes.settings) :
+                o.showSettings = ButtonState.Active;
+                break;
+        }
+    }
+}
+
+function enableGamePageButtonsBasedOnState(props : NavigationSectionProps, o : NavigationOptions) : void {
+    if (!props.game) { return; }
+
+    switch (props.game.status) {
+        case GameStatus.InProgress:
+            o.showPlay = ButtonState.Inactive;
+            o.showDiplomacy = ButtonState.Inactive;
+            break;
+        case GameStatus.Over:
+            o.showGameOver = ButtonState.Inactive;
+            break;
     }
 
-    else if (route === Routes.dashboard) {
-        o.showHome = ButtonState.Active;
-        o.showSettings = ButtonState.Inactive;
-        o.showCreateGame = ButtonState.Inactive;
+    if (props.user && props.user.privileges.includes(Privilege.Snapshots)) {
+        o.showSnapshots = ButtonState.Inactive;
     }
+}
 
-    else if (route === Routes.createGame) {
-        o.showCreateGame = ButtonState.Active;
-        o.showSettings = ButtonState.Inactive;
-        o.showHome = ButtonState.Inactive;
-    }
-
-    else if (route === Routes.settings) {
-        o.showSettings = ButtonState.Active;
-        o.showHome = ButtonState.Inactive;
-        o.showCreateGame = ButtonState.Inactive;
-    }
-
+function getGameId(props : NavigationSectionProps, o : NavigationOptions) : void {
+    const loc : string = props.location.pathname;
     let gameId : number = null;
-    let gamePage : string = null;
-
-    if (route.startsWith("/games")) {
-        const parts = route.split("/");
-        //parts[0] is ""
-        //parts[1] is "games"
-        gameId = Number(parts[2]);
-        gamePage = parts[3];
+    if (loc.startsWith("/games")) {
+        const parts = loc.split("/");
+        gameId = Number(parts[2]); //[ "", "games", "{id}" "{page}" ]
     } else if (props.game) {
         gameId = props.game.id
     }
+    o.gameId = gameId;
+}
 
-    if (gameId) {
-        o.gameId = gameId;
-        o.showHome = ButtonState.Inactive;
-        o.showSettings = ButtonState.Inactive;
-        o.showLobby = ButtonState.Inactive;
-
-        if(props.game) {
-            switch (props.game.status) {
-                case GameStatus.InProgress:
-                    o.showPlay = ButtonState.Inactive;
-                    o.showDiplomacy = ButtonState.Inactive;
-                    break;
-                case GameStatus.Over:
-                    o.showGameOver = ButtonState.Inactive;
-                    break;
-            }
-        }
-        if (props.user && props.user.privileges.includes(Privilege.Snapshots)) {
-            o.showSnapshots = ButtonState.Inactive;
-        }
-
-        switch(gamePage) {
-            case "lobby": {
-                o.showLobby = ButtonState.Active;
-                break;
-            }
-            case "play": {
-                o.showPlay = ButtonState.Active;
-                break;
-            }
-            case "diplomacy": {
-                o.showDiplomacy = ButtonState.Active;
-                break;
-            }
-            case "snapshots": {
-                o.showSnapshots = ButtonState.Active;
-                break;
-            }
-            case "results": {
-                o.showGameOver = ButtonState.Active;
-                break;
-            }
-        }
-    }
-
+function getOptionsFromProps(props : NavigationSectionProps) : NavigationOptions {
+    let o = { ...defaultOptions };
+    applyDefaultsForContext(props, o);
+    enableGamePageButtonsBasedOnState(props, o);
+    markCurrentPageActive(props, o);
+    getGameId(props, o);
     return o;
 }
 
