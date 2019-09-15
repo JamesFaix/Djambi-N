1f0aa8a187bbf833c5e3f1f7ce78773bd2d66452
James Faix <jamesthefaix@gmail.com>
2019-09-14 21:15:31 -0400

    Fixed refs errors in console

--- web/src/components/pageSections/playPageBoardSection.tsx
+++ web/src/components/pageSections/playPageBoardSection.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import CanvasBoard from '../canvas/canvasBoard';
-import { State as AppState } from '../../store/root';
+import { State as AppState, CustomAction } from '../../store/root';
 import { useSelector, useDispatch } from 'react-redux';
 import CanvasTransformService, { CanvasTranformData } from '../../viewModel/board/canvasTransformService';
 import { Classes } from '../../styles/styles';
@@ -8,16 +8,18 @@ import Controller from '../../controllers/controller';
 import Selectors from '../../selectors';
 import Scrollbars, { positionValues } from 'react-custom-scrollbars';
 import Geometry from '../../viewModel/board/geometry';
-import { Point } from '../../viewModel/board/model';
 import * as StoreDisplay from '../../store/display';
 import { ZoomSlider } from '../controls/zoomSlider';
+import { Dispatch } from 'redux';
 
 export const BoardSection : React.SFC<{}> = _ => {
+    const scrollbars = React.useRef(null);
     const game = Selectors.game();
     const board = useSelector((state : AppState) => state.activeGame.boardView);
     const display = useSelector((state : AppState) => state.display);
     const debugSettings = useSelector((state : AppState) => state.settings.debug);
     const dispatch = useDispatch();
+  //  React.useEffect(() => onLoad(display, dispatch));
 
     //React will blow up if you don't call the same hooks in the same order during each render
     //so no early termination until all hooks are used
@@ -31,16 +33,15 @@ export const BoardSection : React.SFC<{}> = _ => {
         regionCount: game.parameters.regionCount
     };
     const internalSize = CanvasTransformService.getSize(transformData);
-
     return (
         <div
             id="board-section"
             className={Classes.boardSection}
         >
-            <BoardScrollArea
-                scrollPercent={display.boardScrollPercent}
-                onScroll={scrollPercent => dispatch(StoreDisplay.Actions.boardScroll(scrollPercent))}
-                onResize={size => dispatch(StoreDisplay.Actions.boardAreaResize(size))}
+            <Scrollbars
+                ref={scrollbars}
+                onScrollFrame={e => onScroll(e, dispatch)}
+                id="board-scroll-area"
             >
                 <CanvasBoard
                     style={{
@@ -56,88 +57,48 @@ export const BoardSection : React.SFC<{}> = _ => {
                     pieceImages={display.images.pieces}
                     debugSettings={debugSettings}
                 />
-            </BoardScrollArea>
-            <BoardZoomSlider
-                zoomLevel={display.boardZoomLevel}
-                scrollPercent={display.boardScrollPercent}
-                onZoom={level => dispatch(StoreDisplay.Actions.boardZoom(level))}
-                scrollAreaSize={display.boardContainerSize}
+            </Scrollbars>
+            <ZoomSlider
+                level={display.boardZoomLevel}
+                changeLevel={level => onZoom(level, display, scrollbars, dispatch)}
             />
         </div>
     );
 }
 
-class BoardScrollArea extends React.Component<{
-    scrollPercent : Point,
-    onScroll : (scrollPercent : Point) => void,
-    onResize : (size : Point) => void
-}> {
-    render() {
-        return (
-            <Scrollbars
-                ref='scrollbar'
-                onScrollFrame={e => this.onScroll(e)}
-                id="board-scroll-area"
-            >
-                {this.props.children}
-            </Scrollbars>
-        );
-    }
-
-    componentDidMount(){
-        const r = this.refs.scrollbar as any;
-        const size = { x: r.getClientWidth(), y: r.getClientHeight() };
-        this.props.onResize(size);
-        this.props.onScroll(this.props.scrollPercent)
-    }
-
-    private onScroll(e : positionValues) : void {
-        const scrollContainerSize = { x: e.clientWidth, y: e.clientHeight };
-        const scrollContentSize = { x: e.scrollWidth, y: e.scrollHeight };
-        const scrollableAreaSize = Geometry.Point.subtract(scrollContentSize, scrollContainerSize);
-
-        const scrollPosition = { x: e.scrollLeft, y: e.scrollTop };
-        const scrollPercent = Geometry.Point.divideSafe(scrollPosition, scrollableAreaSize);
-
-        this.props.onScroll(scrollPercent);
-    }
+function onLoad(display : StoreDisplay.State, dispatch : Dispatch) {
+    dispatch(StoreDisplay.Actions.boardScroll(display.boardScrollPercent));
+    dispatch(StoreDisplay.Actions.boardZoom(display.boardZoomLevel));
 }
 
-class BoardZoomSlider extends React.Component<{
-    zoomLevel : number,
-    scrollPercent : Point,
-    onZoom : (level:number) => void,
-    scrollAreaSize : Point
-}> {
-    render() {
-        return (
-            <ZoomSlider
-                level={this.props.zoomLevel}
-                changeLevel={level => this.onZoom(level)}
-            />
-        );
+function onScroll(e : positionValues, dispatch : Dispatch) {
+    const scrollContainerSize = { x: e.clientWidth, y: e.clientHeight };
+    const scrollContentSize = { x: e.scrollWidth, y: e.scrollHeight };
+    const scrollPosition = { x: e.scrollLeft, y: e.scrollTop };
+    const scrollableAreaSize = Geometry.Point.subtract(scrollContentSize, scrollContainerSize);
+    const scrollPercent = Geometry.Point.divideSafe(scrollPosition, scrollableAreaSize);
+    dispatch(StoreDisplay.Actions.boardScroll(scrollPercent));
+}
+
+function onZoom(level : number, display : StoreDisplay.State, scrollbars : any, dispatch : Dispatch) {
+    const oldLevel = display.boardZoomLevel;
+    const newState : any = { zoomLevel : level };
+
+    //If going from a negative level (where the whole board is visible) to a positive one, start zoom focus at center
+    if (oldLevel <= 0) {
+        newState.scrollPercent = { x: 0.5, y: 0.5 };
     }
 
-    private onZoom(level : number) {
-        const oldLevel = this.props.zoomLevel;
-        const newState : any = { zoomLevel : level };
+    dispatch(StoreDisplay.Actions.boardZoom(level));
 
-        //If going from a negative level (where the whole board is visible) to a positive one, start zoom focus at center
-        if (oldLevel <= 0) {
-            newState.scrollPercent = { x: 0.5, y: 0.5 };
-        }
+    const sb = scrollbars.current;
 
-        this.props.onZoom(level);
+    const scrollContainerSize = { x: sb.getClientWidth(), y: sb.getClientHeight() };
+    const scrollableAreaSize = Geometry.Point.subtract(display.boardContainerSize, scrollContainerSize);
 
-        const {scrollbar} = this.refs as any;
+    const scrollPercent = display.boardScrollPercent;
+    const scrollPosition = Geometry.Point.multiply(scrollPercent, scrollableAreaSize);
 
-        const scrollContainerSize = { x: scrollbar.getClientWidth(), y: scrollbar.getClientHeight() };
-        const scrollableAreaSize = Geometry.Point.subtract(this.props.scrollAreaSize, scrollContainerSize);
-
-        const scrollPercent = this.props.scrollPercent;
-        const scrollPosition = Geometry.Point.multiply(scrollPercent, scrollableAreaSize);
-
-        scrollbar.scrollLeft(scrollPosition.x);
-        scrollbar.scrollTop(scrollPosition.y);
-    }
+    sb.scrollLeft(scrollPosition.x);
+    sb.scrollTop(scrollPosition.y);
 }
