6d7cee5ba5f68cae74ffbd73ab325172b53be471
James Faix <jamesthefaix@gmail.com>
2019-09-18 09:10:31 -0400

    Added file serving to API

--- api/api.host/Program.fs
+++ api/api.host/Program.fs
@@ -1,6 +1,7 @@
 module Djambi.Api.Host.App
 
 open System
+open System.IO
 open System.Linq
 open Giraffe
 open Microsoft.AspNetCore.Builder
@@ -15,6 +16,9 @@ open Djambi.Api.Db
 open Djambi.Api.Logic
 open Djambi.Api.Web
 open Djambi.Utilities
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.FileProviders
+open Microsoft.AspNetCore.Http
 
 // ---------------------------------
 // Error handler
@@ -53,9 +57,35 @@ let configureNewtonsoft () =
     settings.Converters <- converters.ToList()
     JsonConvert.DefaultSettings <- (fun () -> settings)
 
-let configureApp (app : IApplicationBuilder) =
-//    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+let configureFileServer(app : IApplicationBuilder) : IApplicationBuilder =
+    app.UseDefaultFiles()
+        .UseWhen(
+            (fun ctx ->
+                let path = ctx.Request.Path.ToString()
+                let isPublic =
+                    path.StartsWith("/resources") ||
+                    path.StartsWith("/dist") ||
+                    path = "/index.html" ||
 
+                    //This has to be allowed in development because of the React development packages.
+                    //TODO: Disable this in release configuration
+                    path.StartsWith("/node_modules")
+
+                if isPublic then
+                    let msg = sprintf "GET %s" path
+                    Console.WriteLine msg
+
+                isPublic
+            ),
+            (fun a ->
+                let clientPath = Path.Combine(env.root, "web")
+                let sfOptions = StaticFileOptions()
+                sfOptions.FileProvider <- new PhysicalFileProvider(clientPath)
+                a.UseStaticFiles(sfOptions) |> ignore
+            )
+        )
+
+let configureApp (app : IApplicationBuilder) =
     //This will only provide custom serialization of responses to clients.
     //Custom deserialization of request bodies does not work that same way in this framework.
     //See HttpUtility for deserialization.
@@ -70,12 +100,14 @@ let configureApp (app : IApplicationBuilder) =
     let webRoot = WebRoot(env.cookieDomain, manRoot, servRoot)
     let routing = RoutingTable(webRoot)
 
-    app.UseGiraffeErrorHandler(errorHandler)
-    //(match env.IsDevelopment() with
-    //| true  -> app.UseDeveloperExceptionPage()
-    //| false -> app.UseGiraffeErrorHandler errorHandler)
+    configureFileServer(app)
+        .UseGiraffeErrorHandler(errorHandler)
+        //(match env.IsDevelopment() with
+        //| true  -> app.UseDeveloperExceptionPage()
+        //| false -> app.UseGiraffeErrorHandler errorHandler)
         .UseCors(configureCors)
         .UseGiraffe(routing.getHandler)
+
 
 let configureServices (services : IServiceCollection) =
     services.AddCors()    |> ignore
@@ -87,10 +119,13 @@ let configureLogging (builder : ILoggingBuilder) =
 
 [<EntryPoint>]
 let main _ =
+    let webRoot = Path.Combine(env.root, "web")
+
     WebHostBuilder()
         .UseConfiguration(config)
         .UseUrls(env.apiAddress)
         .UseKestrel()
+        .UseWebRoot(webRoot)
         .ConfigureServices(configureServices)
         .Configure(Action<IApplicationBuilder> configureApp)
         .ConfigureLogging(configureLogging)
