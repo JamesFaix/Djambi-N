d84952d1856f0a1d614a2a281735c99122ae4d2b
James Faix <jamesthefaix@gmail.com>
2019-10-03 23:44:50 -0400

    Cleaned up dbreset config

--- utils/db-reset/Program.fs
+++ utils/db-reset/Program.fs
@@ -1,31 +1,11 @@
 open System
 open System.Data.SqlClient
 open System.IO
-open System.Reflection
 open System.Text.RegularExpressions
-
 open Dapper
+open Djambi.Utilities.DbReset
 
-open Microsoft.Extensions.Configuration
-
-let private config =
-    ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", false)
-        .AddEnvironmentVariables("DJAMBI_")
-        .Build()
-
-let private getSqlDirectory =
-    let asm = Assembly.GetExecutingAssembly()
-    let uri = new Uri(asm.CodeBase)
-    let asmDir = uri.LocalPath |> Path.GetDirectoryName
-    let relativeDir = config.["sqlDirectoryRelativePath"]
-    Path.Combine(asmDir, relativeDir)
-
-let private getConnectionString name =
-    config.GetConnectionString(name).Replace("{sqlAddress}", config.["sqlAddress"])
-
-let private masterConnectionString = getConnectionString "master"
-let private djambiConnectionString = getConnectionString "djambi"
+let options = Config.options
 
 let private executeCommand (cnStr : string)(command : string) : unit =
     use cn = new SqlConnection(cnStr)
@@ -38,16 +18,16 @@ let private dropAndCreateDb() : unit =
                DROP DATABASE Djambi;
                CREATE DATABASE Djambi;
                ALTER DATABASE Djambi SET COMPATIBILITY_LEVEL = 120" //120 = SQL Server 2014
-    executeCommand masterConnectionString sql
+    executeCommand options.masterConnectionString sql
 
 let private loadFile (relativePath : string) : unit =
     printfn "Loading %s" relativePath
-    let absolutePath = Path.Combine(getSqlDirectory, relativePath)
+    let absolutePath = Path.Combine(options.sqlRoot, relativePath)
     let text = File.ReadAllText(absolutePath)
     let commands = Regex.Split(text, "\s+GO")
                    |> Seq.filter (String.IsNullOrEmpty >> not)
     for c in commands do
-        executeCommand djambiConnectionString c
+        executeCommand options.djambiConnectionString c
 
 let getFilesInOrder : string seq =
     seq {
@@ -77,7 +57,7 @@ let getFilesInOrder : string seq =
         yield! tables |> Seq.map (fun name -> sprintf "Tables\\dbo.%s.sql" name)
 
         let getFiles folder =
-            Path.Combine(getSqlDirectory, folder)
+            Path.Combine(options.sqlRoot, folder)
             |> Directory.EnumerateFiles
             |> Seq.map (fun path -> Path.Combine(folder, Path.GetFileName(path)))
 
@@ -92,7 +72,7 @@ let getFilesInOrder : string seq =
     }
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     dropAndCreateDb()
 
     for f in getFilesInOrder do
