d2aaa13909232557e225f18fbdad5aada7509f41
James Faix <jamesthefaix@gmail.com>
2019-10-04 00:09:12 -0400

    Refactored API config

--- api/api.host/Program.fs
+++ api/api.host/Program.fs
@@ -7,7 +7,6 @@ open Giraffe
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Cors.Infrastructure
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.FileProviders
 open Microsoft.Extensions.Logging
@@ -18,14 +17,10 @@ open Djambi.Api.Db
 open Djambi.Api.Logic
 open Djambi.Api.Web
 
-let config = 
-    ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", false)
-        .AddEnvironmentVariables("DJAMBI_")
-        .Build()
+let options = Djambi.Api.Host.Config.options
 
 let log = 
-    let logPath = Path.Combine(config.["logsDir"], "server.log")
+    let logPath = Path.Combine(options.logsDir, "server.log")
     LoggerConfiguration()
         .WriteTo.File(logPath)
         .WriteTo.Console()
@@ -38,15 +33,11 @@ let errorHandler (ex : Exception) (logger : Microsoft.Extensions.Logging.ILogger
     log.Error(ex, "An unexpected error occurred.")
     clearResponse >=> setStatusCode 500 >=> text ex.Message
 
-let enableWebServer = config.GetValue<bool>("AppSettings:EnableWebServer")
-let enableWebServerDevelopmentMode = config.GetValue<bool>("AppSettings:EnableWebServerDevelopmentMode")
-let webRoot = config.["webRoot"]
-
-if enableWebServer then
-    log.Information(sprintf "Web root path: %s" webRoot)
+if options.enableWebServer then
+    log.Information(sprintf "Web root path: %s" options.webRoot)
 
 let configureCors (builder : CorsPolicyBuilder) =
-    builder.WithOrigins(config.["webAddress"])
+    builder.WithOrigins(options.webAddress)
            .AllowAnyMethod()
            .AllowAnyHeader()
            .AllowCredentials()
@@ -77,7 +68,7 @@ let configureWebServer(app : IApplicationBuilder) : IApplicationBuilder =
                 path.StartsWith("/dist") ||
                 isDefault path
 
-            if enableWebServerDevelopmentMode then
+            if options.enableWebServerDevelopmentMode then
                 //This has to be allowed in development because of the React development packages.
                 //TODO: Disable this in release configuration
                 isPublic <- isPublic || path.StartsWith("/node_modules")
@@ -89,7 +80,7 @@ let configureWebServer(app : IApplicationBuilder) : IApplicationBuilder =
         ),
         (fun a ->
             let sfOptions = StaticFileOptions()
-            sfOptions.FileProvider <- new PhysicalFileProvider(webRoot)
+            sfOptions.FileProvider <- new PhysicalFileProvider(options.webRoot)
             
             a.UseDefaultFiles()
                 .UseStaticFiles(sfOptions) 
@@ -98,13 +89,10 @@ let configureWebServer(app : IApplicationBuilder) : IApplicationBuilder =
     )
 
 let apiHandler =
-    let connStr = config.GetConnectionString("Main")
-                        .Replace("{sqlAddress}", config.["sqlAddress"])
-
-    let dbRoot = DbRoot(connStr)
+    let dbRoot = DbRoot(options.djambiConnectionString)
     let servRoot = ServiceRoot(dbRoot, log)
     let manRoot = ManagerRoot(dbRoot, servRoot)
-    let webRoot = WebRoot(config.["cookieDomain"], manRoot, servRoot, log)
+    let webRoot = WebRoot(options.cookieDomain, manRoot, servRoot, log)
     let routing = RoutingTable(webRoot)
 
     routing.getHandler
@@ -120,7 +108,7 @@ let configureApp (app : IApplicationBuilder) =
 
     configureNewtonsoft()
 
-    (if enableWebServer then configureWebServer app else app)
+    (if options.enableWebServer then configureWebServer app else app)
         .UseGiraffeErrorHandler(errorHandler)
         .UseCors(configureCors)
         .UseWebSockets()
@@ -131,10 +119,9 @@ let configureApp (app : IApplicationBuilder) =
 [<EntryPoint>]
 let main _ =
     (WebHostBuilder()
-        .UseConfiguration(config)
-        .UseUrls(config.["apiAddress"])
+        .UseUrls(options.apiAddress)
         .UseKestrel()
-        |> (fun b -> if enableWebServer then b.UseWebRoot(webRoot) else b)
+        |> (fun b -> if options.enableWebServer then b.UseWebRoot(options.webRoot) else b)
     )
         .ConfigureServices(configureServices)
         .Configure(Action<IApplicationBuilder> configureApp)
