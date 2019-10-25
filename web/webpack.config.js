7ac276d57156a2beb5d2aa3d0efd767ca59edff1
James Faix <jamesthefaix@gmail.com>
2019-10-24 20:08:32 -0400

    Copying all files for dist folder

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = () => {
     return {
@@ -48,7 +49,26 @@ module.exports = () => {
               'process.env':{
                 API_URL: JSON.stringify(process.env.DJAMBI_apiAddress)
               }
-            })
+            }),
+            new CopyWebpackPlugin([
+                {
+                    from: "index.html",
+                    to: "index.html"
+                },
+                {
+                    from: "resources",
+                    to: "resources",
+                    ignore: [ "**/*.psd" ]
+                },
+                {
+                    from: "node_modules/react/umd/react.development.js",
+                    to: "node_modules/react/umd/react.development.js"
+                },
+                {
+                    from: "node_modules/react-dom/umd/react-dom.development.js",
+                    to: "node_modules/react-dom/umd/react-dom.development.js"
+                }
+            ])
         ]
     };
 };
