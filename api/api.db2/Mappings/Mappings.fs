b167f5fcf423cf52ce379c854e32148d23656a3f
James Faix <jamesthefaix@gmail.com>
2020-03-24 23:39:30 -0400

    Implemented most of UserRepository with EF

---
+++ api/api.db2/Mappings/Mappings.fs
@@ -0,0 +1,24 @@
+﻿module Apex.Api.Db.Mappings
+
+open Apex.Api.Db.Model
+open Apex.Api.Model
+open System.ComponentModel
+
+let toPrivilege (source : PrivilegeSqlModel) : Privilege =
+    match source.Id with
+    | 1uy -> Privilege.EditUsers
+    | 2uy -> Privilege.EditPendingGames
+    | 3uy -> Privilege.OpenParticipation
+    | 4uy -> Privilege.Snapshots
+    | 5uy -> Privilege.ViewGames
+    | _ -> raise <| InvalidEnumArgumentException()
+
+let toUserDetails (source : UserSqlModel) : UserDetails =
+    {
+        id = source.Id
+        name = source.Name
+        password = source.Password
+        failedLoginAttempts = int source.FailedLoginAttempts
+        lastFailedLoginAttemptOn = source.LastFailedLoginAttemptOn |> Option.ofNullable
+        privileges = source.Privileges |> Seq.map toPrivilege |> Seq.toList
+    }    
---
+++ api/api.db2/Repositories/Repositories.fs
@@ -0,0 +1,57 @@
+﻿namespace Apex.Api.Db.Repositories
+
+open Apex.Api.Db.Interfaces
+open System
+open Apex.Api.Db.Model
+open FSharp.Control.Tasks
+open Apex.Api.Common.Control
+open Apex.Api.Db.Mappings
+open Microsoft.EntityFrameworkCore
+
+type UserRepository(context : ApexDbContext) =    
+    interface IUserRepository with
+        member __.getUser userId =
+            task {
+                let! sqlModel = context.Users.FindAsync(userId)
+
+                return match sqlModel with
+                        | null -> Error <| HttpException(400, "Not found.")
+                        | x -> Ok(x |> toUserDetails)
+            }
+
+        member __.getUserByName name =
+            task {
+                let! sqlModel = context.Users.SingleOrDefaultAsync(fun x -> 
+                    String.Equals(name, x.Name, StringComparison.InvariantCultureIgnoreCase))
+
+                return match sqlModel with
+                        | null -> Error <| HttpException(400, "Not found.")
+                        | x -> Ok(x |> toUserDetails)
+            }
+
+        member __.createUser request =
+            task {
+                let u = UserSqlModel()
+                u.Name <- request.name
+                u.Password <- request.password
+                u.FailedLoginAttempts <- 0uy
+                u.LastFailedLoginAttemptOn <- Nullable<DateTime>()
+                u.CreatedOn <- DateTime.UtcNow
+                u.Privileges <- System.Collections.Generic.List<PrivilegeSqlModel>()
+
+                let! _ = context.Users.AddAsync(u)
+                let! _ = context.SaveChangesAsync()
+
+                return u |> toUserDetails |> Ok
+            }
+
+        member __.deleteUser userId = 
+            task {
+                let! u = context.Users.FindAsync(userId)
+                context.Users.Remove(u) |> ignore
+                let! _ = context.SaveChangesAsync()
+                return Ok ()
+            }
+
+        member __.updateFailedLoginAttempts request =
+            raise <| NotImplementedException()
