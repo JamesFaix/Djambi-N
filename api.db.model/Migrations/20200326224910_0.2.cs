d87fa9783b8e7c186a43ab102cc94b61b973eaea
James Faix <jamesthefaix@gmail.com>
2020-03-26 18:49:28 -0400

    Added users table (without privileges relation)

---
+++ api.db.model/Migrations/20200326224910_0.2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Apex.Api.Db.Model.Migrations
+{
+    public partial class _02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    FailedLoginAttempts = table.Column<byte>(nullable: false),
+                    LastFailedLoginAttemptOn = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- api.db.model/Model/UserSqlModel.cs
+++ api.db.model/Model/UserSqlModel.cs
@@ -1,31 +1,31 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations;
-//using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-//namespace Apex.Api.Db.Model
-//{
-//    [Table("Users")]
-//    public class UserSqlModel
-//    {
-//        [Required]
-//        public int Id { get; set; }
+namespace Apex.Api.Db.Model
+{
+    [Table("Users")]
+    public class UserSqlModel
+    {
+        [Required]
+        public int Id { get; set; }
 
-//        [Required] 
-//        [StringLength(20)]
-//        public string Name { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
 
-//        [Required]
-//        public string Password { get; set; }
+        [Required]
+        public string Password { get; set; }
 
-//        [Required] 
-//        public DateTime CreatedOn { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
 
-//        [Required] 
-//        public byte FailedLoginAttempts { get; set; }
+        [Required]
+        public byte FailedLoginAttempts { get; set; }
 
-//        public DateTime? LastFailedLoginAttemptOn { get; set; }
+        public DateTime? LastFailedLoginAttemptOn { get; set; }
 
-//        public List<UserPrivilegeSqlModel> UserPrivileges { get; set; }
-//    }
-//}
+        //public List<UserPrivilegeSqlModel> UserPrivileges { get; set; }
+    }
+}
