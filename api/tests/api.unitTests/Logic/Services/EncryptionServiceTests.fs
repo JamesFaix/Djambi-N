6803e2cdb3632ff40a622cd7f46cc4e79cc5f7b7
James Faix <jamesthefaix@gmail.com>
2020-07-24 22:22:49 -0400

    Added tests for encryptionservice

---
+++ api/tests/api.unitTests/Logic/Services/EncryptionServiceTests.fs
@@ -0,0 +1,27 @@
+﻿module Apex.Api.UnitTests.EncryptionServiceTests
+
+open Xunit
+open Apex.Api.Logic.Interfaces
+open Apex.Api.Logic.Services
+
+let service = EncryptionService() :> IEncryptionService
+
+[<Fact>]
+let ``hash does not return input``() =
+    let password = "abc"
+    let hash = service.hash password
+    Assert.NotEqual<string>(password, hash)
+
+[<Fact>]
+let ``check returns not verified if password and hash do not match`` =
+    let password = "abc"
+    let hash = "123"
+    let result = service.check(hash, password)
+    Assert.False result.verified
+
+[<Fact>]
+let ``check return verified if password matches hash`` = 
+    let password = "abc"
+    let hash = service.hash password
+    let result = service.check(hash, password)
+    Assert.True result.verified
