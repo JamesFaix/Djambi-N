882946e025b2a369d0e42fbc14049998ebc1b436
James Faix <jamesthefaix@gmail.com>
2020-07-29 12:16:02 -0400

    Added logging to session lookup for triage

--- api/api.web/SessionContextProvider.fs
+++ api/api.web/SessionContextProvider.fs
@@ -1,38 +1,47 @@
 ﻿namespace Apex.Api.Web
 
 open System
+open System.Security.Authentication
 open System.Threading.Tasks
+open FSharp.Control.Tasks
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Options
-
-open FSharp.Control.Tasks
-
+open Serilog
 open Apex.Api.Common.Control
 open Apex.Api.Logic.Interfaces
 open Apex.Api.Model
 open Apex.Api.Model.Configuration
-open System.Security.Authentication
 
 type SessionContextProvider(options : IOptions<ApiSettings>,
                             service : ISessionService,
-                            cookieProvider : CookieProvider) =
+                            cookieProvider : CookieProvider,
+                            logger : ILogger) =
     
     member x.GetSessionOptionFromContext (ctx : HttpContext) : Task<Option<Session>> =
+
         let cookieName = options.Value.cookieName
         let token = ctx.Request.Cookies.Item(cookieName)
 
+        logger.Information("GetSessionOptionFromContext {cookieName} {token}", cookieName, token)
+
         if token |> String.IsNullOrEmpty
-        then None |> Task.FromResult
+        then 
+            logger.Information("No token")
+            None |> Task.FromResult
         else
             task {
                 try 
                     match! service.getAndRenewSession token with
-                    | Some session -> return Some session
+                    | Some session -> 
+                        logger.Information("Found session matching token")
+                        return Some session
                     | None ->
+                        logger.Information("No session matching token")
                         cookieProvider.AppendEmptyCookie ctx
                         return None
                 with
                 | _ as ex ->
+                    logger.Error(ex, "Error finding session")
                     cookieProvider.AppendEmptyCookie ctx
                     return raise ex
             }
