c293d9bd02a96a41683b52eb1058ebf3f7a89a45
James Faix <jamesthefaix@gmail.com>
2020-08-04 22:44:30 -0400

    Split up components into separate files

--- web2/src/components/NavigationDrawer/NavigationDrawer.tsx
+++ web2/src/components/NavigationDrawer/NavigationDrawer.tsx
@@ -1,74 +1,12 @@
-import React, { FC } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import {
-  AccountBalance as DiplomacyIcon,
-  Add as NewGameIcon,
-  CameraAlt as SnapshotsIcon,
-  EmojiEvents as ResultsIcon,
-  ExitToApp as SignOutIcon,
-  Gavel as RulesIcon,
-  Home as HomeIcon,
-  Input as SignInIcon,
-  MeetingRoom as LobbyIcon,
-  PersonAdd as CreateAccountIcon,
-  PlayArrow as PlayIcon,
-  Search as SearchIcon,
-  Settings as SettingsIcon
-} from '@material-ui/icons';
+import { Button, Divider, Drawer } from '@material-ui/core';
 import { GameInfo } from '../../model/game';
-import { navigateTo } from '../../utilities/navigation';
 import { PlayerKind } from '../../api-client';
-
-interface NavigationItemProps {
-  text: string;
-  icon: any;
-  onClick: () => void;
-}
-
-const NavigationItem: FC<NavigationItemProps> = ({ text, icon, onClick }) => {
-  return (
-    <ListItem button key={text} onClick={onClick} >
-      <ListItemIcon>{icon}</ListItemIcon>
-      <ListItemText primary={text} />
-    </ListItem>
-  );
-};
-
-const UnauthenticatedNavigationItems: FC = () => {
-  return (
-    <List>
-      <NavigationItem text="Sign in" icon={<SignInIcon />} onClick={() => navigateTo('/sign-in')} />
-      <NavigationItem text="Create account" icon={<CreateAccountIcon />} onClick={() => navigateTo('/create-account')} />
-    </List>
-  );
-};
-
-const GamelessNavigationItems: FC = () => {
-  return (
-    <List>
-      <NavigationItem text="Home" icon={<HomeIcon />} onClick={() => navigateTo('/home')} />
-      <NavigationItem text="Settings" icon={<SettingsIcon />} onClick={() => navigateTo('/settings')} />
-      <NavigationItem text="New game" icon={<NewGameIcon />} onClick={() => navigateTo('/new-game')} />
-      <NavigationItem text="Search games" icon={<SearchIcon />} onClick={() => navigateTo('/search-games')} />
-      <NavigationItem text="Rules" icon={<RulesIcon />} onClick={() => navigateTo('/rules')} />
-      <NavigationItem text="Sign out" icon={<SignOutIcon />} onClick={() => navigateTo('/sign-out')} />
-    </List>
-  );
-};
-
-const GameNavigationItems: FC<{ game: GameInfo }> = ({ game }) => {
-  return (
-    <List>
-      <NavigationItem text="Game lobby" icon={<LobbyIcon />} onClick={() => navigateTo(`/games/${game.id}/lobby`)} />
-      <NavigationItem text="Resume game" icon={<PlayIcon />} onClick={() => navigateTo(`/games/${game.id}/play`)} />
-      <NavigationItem text="Game diplomacy" icon={<DiplomacyIcon />} onClick={() => navigateTo(`/games/${game.id}/diplomacy`)} />
-      <NavigationItem text="Game snapshots" icon={<SnapshotsIcon />} onClick={() => navigateTo(`/games/${game.id}/snapshots`)} />
-      <NavigationItem text="Game outcome" icon={<ResultsIcon />} onClick={() => navigateTo(`/games/${game.id}/outcome`)} />
-    </List>
-  );
-};
+import UnauthenticatedNavigationItems from './UnauthenticatedNavigationItems';
+import GamelessNavigationItems from './GamelessNavigationItems';
+import ActiveGameNavigationItems from './ActiveGameNavigationItems';
 
 const useStyles = makeStyles({
   list: {
@@ -118,7 +56,7 @@ export default function TemporaryDrawer() {
       <Divider />
       <GamelessNavigationItems />
       <Divider />
-      <GameNavigationItems game={game} />
+      <ActiveGameNavigationItems game={game} />
     </div >
   );
 
