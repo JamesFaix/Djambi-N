53e7c8381ac14377365d903832cd9dad1be3d051
James Faix <jamesthefaix@gmail.com>
2020-08-06 10:53:19 -0400

    Updated pipelines

---
+++ rest-client-generator/check-if-client-changed.ps1
@@ -0,0 +1,15 @@
+$diff = git diff --stat
+if ($null -eq $diff) {
+    Write-Host "Found 0 files in diff."
+}
+else {
+    $clientFiles = $diff | Where-Object {$_.Trim().StartsWith("web2/src/api-client/")}
+
+    if ($clientFiles.count -gt 0) {
+        Write-Host "Found $($diff.count) files in diff, including $($clientFiles.count) API client files." -ForegroundColor Red
+        throw "API client has not been regenerated since last API contract changes."
+    }
+    else {
+        Write-Host "Found $($diff.count) files in diff, but no API client files."
+    }
+}
