863f77b8e61b1cb4a40a0e67f9af6ac0f24d8abe
James Faix <jamesthefaix@gmail.com>
2020-08-09 22:13:50 -0400

    Fixed test

--- web2/src/components/App/App.test.tsx
+++ web2/src/components/App/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../redux';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = getByText(/API URL/i);
   expect(linkElement).toBeInTheDocument();
 });
