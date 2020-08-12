8b6abdffa4ba1f1fd0520ac89e9d7874dfdb5bbd
James Faix <jamesthefaix@gmail.com>
2020-08-11 21:58:40 -0400

    Weakened example test to work with layout changes

--- web2/src/components/App/App.test.tsx
+++ web2/src/components/App/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from '../../redux';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
-  const linkElement = getByText(/API URL/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App/>', () => {
+  it('renders', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+  });
 });
