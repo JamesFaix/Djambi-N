978ce026cd169d3c57086d9d97e0dda1bc6a0d44
James Faix <jamesthefaix@gmail.com>
2020-08-13 12:39:37 -0400

    Fixed redirect logic

--- web2/src/components/routing/RedirectBasedOnStore.tsx
+++ web2/src/components/routing/RedirectBasedOnStore.tsx
@@ -2,12 +2,14 @@ import { useSelector } from 'react-redux';
 import React, { FC } from 'react';
 import { useLocation, Redirect } from 'react-router-dom';
 import { selectNavigation } from '../../hooks/selectors';
+import { defaultNavigationState } from '../../redux/navigation/state';
 
 const RedirectBasedOnStore: FC = () => {
   const navigation = useSelector(selectNavigation);
   const location = useLocation();
 
-  if (navigation.path !== location.pathname) {
+  if (navigation !== defaultNavigationState
+    && navigation.path !== location.pathname) {
     return (
       <Redirect to={navigation.path} />
     );
