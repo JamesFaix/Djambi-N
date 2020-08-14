f28d8d86dd22d49c492273a6151e404bf04d8b0e
James Faix <jamesthefaix@gmail.com>
2020-08-13 21:40:32 -0400

    Added game load to game pages

--- web2/src/components/pages/GameDiplomacyPage.tsx
+++ web2/src/components/pages/GameDiplomacyPage.tsx
@@ -1,12 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import RedirectToSignInIfSignedOut from '../routing/RedirectToSignInIfSignedOut';
 import { GamePageProps } from './GamePage';
+import { loadGame } from '../../controllers/gameController';
+import { selectActiveGame } from '../../hooks/selectors';
 
 const GameDiplomacyPage: FC<GamePageProps> = ({ gameId }) => {
+  const state = useSelector(selectActiveGame);
+
+  useEffect(() => {
+    if (state.game === null) {
+      loadGame(gameId);
+    }
+  });
+
   return (
     <div>
       <RedirectToSignInIfSignedOut />
       {`Game ${gameId} diplomacy page`}
+      <br />
+      {state.game ? JSON.stringify(state.game) : ''}
     </div>
   );
 };
--- web2/src/components/pages/GamePage.tsx
+++ web2/src/components/pages/GamePage.tsx
@@ -1,15 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import RedirectToSignInIfSignedOut from '../routing/RedirectToSignInIfSignedOut';
+import { selectActiveGame } from '../../hooks/selectors';
+import { loadGame } from '../../controllers/gameController';
+import { GameStatus } from '../../api-client';
+import { navigateTo } from '../../utilities/navigation';
+import * as Routes from '../../utilities/routes';
 
 export interface GamePageProps {
   gameId: number
 }
 
 const GamePage: FC<GamePageProps> = ({ gameId }) => {
+  const state = useSelector(selectActiveGame);
+
+  useEffect(() => {
+    if (state.game === null) {
+      loadGame(gameId);
+      return;
+    }
+
+    switch (state.game.status) {
+      case GameStatus.InProgress:
+        navigateTo(Routes.gamePlay(gameId));
+        return;
+      case GameStatus.Canceled:
+      case GameStatus.Pending:
+        navigateTo(Routes.gameLobby(gameId));
+        return;
+      case GameStatus.Over:
+        navigateTo(Routes.gameOutcome(gameId));
+        return;
+      default:
+        throw new Error(`Invalid game status ${state.game.status}`);
+    }
+  });
+
   return (
     <div>
       <RedirectToSignInIfSignedOut />
       {`Game ${gameId} page`}
+      <br />
+      {state.game ? JSON.stringify(state.game) : ''}
     </div>
   );
 };
