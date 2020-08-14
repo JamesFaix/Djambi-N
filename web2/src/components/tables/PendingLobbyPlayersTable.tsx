750f280d58f2c22b8d5a9ac4560057d0a653c539
James Faix <jamesthefaix@gmail.com>
2020-08-14 12:28:11 -0400

    Extracted row into separate component

--- web2/src/components/tables/PendingLobbyPlayersTable.tsx
+++ web2/src/components/tables/PendingLobbyPlayersTable.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
-  Table, TableContainer, TableHead, TableCell, TableRow, TableBody,
+  Table, TableContainer, TableHead, TableCell, TableRow, TableBody, TextField,
 } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import { selectActiveGame, selectSession } from '../../hooks/selectors';
@@ -74,6 +74,54 @@ function getViewModels(currentUser: UserDto, game: GameDto): PlayerSlotViewModel
   return players.concat(emptySlots);
 }
 
+interface RowProps {
+  player: PlayerSlotViewModel,
+  game: GameDto
+}
+
+const Row: FC<RowProps> = ({ player, game }) => {
+  const controlStyle = {
+    padding: '10px',
+  };
+
+  const [name, setName] = useState('');
+
+  let helperText = '';
+
+  const isDuplicateName = game.players.find((p) => p.name === name);
+  if (isDuplicateName) {
+    helperText = `There is already a player named ${name}`;
+  }
+
+  const isBlank = name === '';
+  if (isBlank) {
+    helperText = 'Name cannot be blank';
+  }
+
+  const isValidName = !isDuplicateName && !isBlank;
+
+  return (
+    <TableRow>
+      <TableCell>{
+        player.actionType === LobbyPlayerActionType.Add
+          ? (
+            <TextField
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              error={!isValidName}
+              helperText={helperText}
+              style={controlStyle}
+            />
+          )
+          : player.name
+      }
+      </TableCell>
+      <TableCell>{player.note}</TableCell>
+      <TableCell>{player.actionType}</TableCell>
+    </TableRow>
+  );
+};
+
 const PendingLobbyPlayersTable: FC = () => {
   const { game } = useSelector(selectActiveGame);
   const { user } = useSelector(selectSession);
@@ -96,13 +144,7 @@ const PendingLobbyPlayersTable: FC = () => {
         </TableHead>
         <TableBody>
           {
-            viewModels.map((p, i) => (
-              <TableRow key={i.toString()}>
-                <TableCell>{p.name}</TableCell>
-                <TableCell>{p.note}</TableCell>
-                <TableCell>{p.actionType}</TableCell>
-              </TableRow>
-            ))
+            viewModels.map((p, i) => <Row key={i.toString()} player={p} game={game} />)
           }
         </TableBody>
       </Table>
