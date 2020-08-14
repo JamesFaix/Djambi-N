5a512216c1aa9350b183b6808349b1fa8a7d8ab5
James Faix <jamesthefaix@gmail.com>
2020-08-13 22:07:37 -0400

    Web2 - Create games #660

--- web2/src/controllers/gameController.ts
+++ web2/src/controllers/gameController.ts
@@ -1,9 +1,19 @@
 import * as Api from '../utilities/api';
 import { gameLoaded } from '../redux/activeGame/actionFactory';
 import { store } from '../redux';
+import { GameParametersDto } from '../api-client';
+import * as Routes from '../utilities/routes';
+import { navigateTo } from '../utilities/navigation';
 
 export async function loadGame(gameId: number): Promise<void> {
   const game = await Api.games().apiGamesGameIdGet({ gameId });
   const action = gameLoaded(game);
   store.dispatch(action);
+}
+
+export async function createGame(parameters: GameParametersDto): Promise<void> {
+  const game = await Api.games().apiGamesPost({ gameParametersDto: parameters });
+  const action = gameLoaded(game);
+  store.dispatch(action);
+  navigateTo(Routes.gameLobby(game.id));
 }
