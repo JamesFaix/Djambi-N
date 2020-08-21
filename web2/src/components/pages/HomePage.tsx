4be6629943ddd4500237041ed02b918f81f79831
James Faix <jamesthefaix@gmail.com>
2020-08-20 21:52:48 -0400

    Got recent games to display

--- web2/src/components/pages/HomePage.tsx
+++ web2/src/components/pages/HomePage.tsx
@@ -1,8 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 import RedirectToSignInIfSignedOut from '../routing/RedirectToSignInIfSignedOut';
+import GameSearchResultsTable from '../tables/GameSearchResultsTable';
+import { SearchGameDto, GameStatus } from '../../api-client';
+import { searchGames } from '../../controllers/searchController';
+import { selectSession } from '../../hooks/selectors';
 
 const HomePage: FC = () => {
+  const [recentGames, setRecentGames] = useState<SearchGameDto[]>([]);
+  const { user } = useSelector(selectSession);
+
+  useEffect(() => {
+    searchGames({
+      playerUserName: user?.name,
+      statuses: [GameStatus.Pending, GameStatus.InProgress],
+    })
+      .then((games) => {
+        const sorted = games.sort((x) => x.id).reverse();
+        setRecentGames(sorted);
+      });
+  });
+
   return (
     <div>
       <RedirectToSignInIfSignedOut />
@@ -10,6 +29,12 @@ const HomePage: FC = () => {
         Home
       </Typography>
       <br />
+      <br />
+      <Typography variant="h5">
+        Recent games
+      </Typography>
+      <br />
+      <GameSearchResultsTable games={recentGames} />
     </div>
   );
 };
