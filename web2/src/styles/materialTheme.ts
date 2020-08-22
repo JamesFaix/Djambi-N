7fb62e568f30e2e5ee5716c11df24eb7886ae3db
James Faix <jamesthefaix@gmail.com>
2020-08-21 22:37:20 -0400

    styling fixes

--- web2/src/styles/materialTheme.ts
+++ web2/src/styles/materialTheme.ts
@@ -11,6 +11,9 @@ export const theme = createMuiTheme({
     h5: {
       color: '#fff',
     },
+    body1: {
+      color: '#fff',
+    },
     caption: {
       color: '#fff',
       opacity: 0.7,
