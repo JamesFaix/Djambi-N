2330c918d0116c3bbde110390a3d1ddfc37b282d
James Faix <jamesthefaix@gmail.com>
2020-08-28 14:59:50 -0400

    Added click event to rows

--- web2/src/components/tables/GameSearchResultsTable.tsx
+++ web2/src/components/tables/GameSearchResultsTable.tsx
@@ -3,6 +3,8 @@ import {
   Table, TableContainer, TableCell, TableRow, TableBody, TableHead,
 } from '@material-ui/core';
 import { SearchGameDto } from '../../api-client';
+import * as Routes from '../../utilities/routes';
+import { navigateTo } from '../../controllers/navigationController';
 
 interface Props {
   games: SearchGameDto[]
@@ -22,7 +24,7 @@ const GameSearchResultsTable: FC<Props> = ({ games }) => {
         <TableBody>
           {
             games.map((g, i) => (
-              <TableRow key={i.toString()}>
+              <TableRow key={i.toString()} onClick={() => navigateTo(Routes.game(g.id))}>
                 <TableCell>{g.id}</TableCell>
                 <TableCell>{g.parameters.description}</TableCell>
                 <TableCell>{g.status}</TableCell>
