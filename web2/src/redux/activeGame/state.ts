ba99e68116e042f95e595e192f436410cc27025a
James Faix <jamesthefaix@gmail.com>
2020-08-28 22:18:45 -0400

    Web2/fix game load (#703)
    * Added property to store for game load pending
    
    * Fixed loading

--- web2/src/redux/activeGame/state.ts
+++ web2/src/redux/activeGame/state.ts
@@ -1,9 +1,11 @@
 import { GameDto } from '../../api-client';
 
 export type ActiveGameState = {
-  game: GameDto | null
+  game: GameDto | null,
+  isLoadPending: boolean,
 };
 
 export const defaultActiveGameState: ActiveGameState = {
   game: null,
+  isLoadPending: false,
 };
